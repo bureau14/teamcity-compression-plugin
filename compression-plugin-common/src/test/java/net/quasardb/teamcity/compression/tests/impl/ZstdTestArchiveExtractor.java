40c442dd2ae543ee1979d973bc3e37d561e0c3e8
Aleksey Alekseev <voodoo144@gmail.com>
2024-10-09 19:24:39 -0400

    Calling to available archive extractors, instead of implementing our own

--- compression-plugin-common/src/test/java/net/quasardb/teamcity/compression/tests/impl/ZstdTestArchiveExtractor.java
+++ compression-plugin-common/src/test/java/net/quasardb/teamcity/compression/tests/impl/ZstdTestArchiveExtractor.java
@@ -1,7 +1,22 @@
 package net.quasardb.teamcity.compression.tests.impl;
 
 import jetbrains.buildServer.ExtensionHolder;
+import jetbrains.buildServer.util.ArchiveExtractor;
+import jetbrains.buildServer.util.impl.TarArchiveExtractor;
+import jetbrains.buildServer.util.impl.ZipArchiveExtractor;
 import net.quasardb.teamcity.compression.ZstdExtractor;
+import net.quasardb.teamcity.compression.logging.Logger;
+import net.quasardb.teamcity.compression.utils.ZstdCompressionUtils;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class ZstdTestArchiveExtractor implements ZstdExtractor {
 
@@ -12,4 +27,21 @@ public class ZstdTestArchiveExtractor implements ZstdExtractor {
         return null;
     }
 
+    @Override
+    public ArchiveExtractor getArchiveType(@NotNull File archive) {
+        Logger.debug("Call isSupportedArchiveType for " + archive.getName());
+        try {
+            try (InputStream inputStream = new BufferedInputStream(Files.newInputStream(archive.toPath()))) {
+                if(ZstdCompressionUtils.isArchiveOfType(inputStream, Collections.singletonList(ArchiveStreamFactory.TAR))){
+                    return new TarArchiveExtractor();
+                }
+                if(ZstdCompressionUtils.isArchiveOfType(inputStream, Collections.singletonList(ArchiveStreamFactory.ZIP))){
+                    return new ZipArchiveExtractor();
+                }
+            }
+        } catch (IOException e) {
+            Logger.error("Caught exception during test of archive", e);
+        }
+        return null;
+    }
 }
